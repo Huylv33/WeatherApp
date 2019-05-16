efd8f8be0876cc1fbd2242a7cddb936250cbb2a5
Lê Văn Huy <huylv@talent.ominext.com>
2019-05-16 17:05:19 +0700

    pull success

--- app/src/main/java/com/project/mobile/weatherapp/fragment_forecast.java
+++ app/src/main/java/com/project/mobile/weatherapp/fragment_forecast.java
@@ -48,13 +48,10 @@ public class fragment_forecast extends Fragment {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setAdapter(mAdapter);
-
         return view;
     }
     private void initView () {
         Daily daily = new Daily("THỨ NĂM, THG 5 16", "Nắng nhẹ", " 36", "26");
         mList.add(daily);
-
-
     }
 }
